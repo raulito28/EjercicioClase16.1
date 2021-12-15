5a810d43c089784d2b1daade8d01c8a2b03c700f
rbuenoh <raulbuenoherrera@gmail.com>
2021-12-15 12:01:32 +0100

    Update

--- main.py
+++ main.py
@@ -2,9 +2,17 @@
 PALABRAS = "palabras.txt"
 FRASES = "frases.txt"
 archivo = open(PALABRAS, "r")
-palabras = []
+palabras = {}
 for linea in archivo:
-    palabras[linea.stip()] = []
+    palabras[linea.strip()] = []
 print("Lista de palabras: \n", palabras)
 
 archivo = open(FRASES, "r")
+frases = []
+for frase in archivo:
+    frases.append(frase.strip())
+    for palabra in palabras:
+        palabras[palabra].append(frase.lower().count(palabra))
+archivo.close()
+print("Frases: \n", frases)
+print("Palabras en frases: \n", palabras)
